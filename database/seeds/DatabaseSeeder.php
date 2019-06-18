a36277e2b3a6c781a4f6c0a440bfad9f7d6a68da
antonina <antoninasych@gmail.com>
2019-06-18 11:19:26 +0300

    seed updated

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,14 +12,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-   // $this->call(UsersTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
 
         $this->call(PermissionTableSeeder::class);
         $this->call(MccCodesSeeder::class);
         $this->call(OrderFieldSeeder::class);
-         $this->call(RefOrderStatusSeeder::class);
-         $this->call(CmsTableSeeder::class);
+        $this->call(RefOrderStatusSeeder::class);
+        $this->call(CmsTableSeeder::class);
         $this->call(UserRole::class);
         $this->call(RefFeeTypeSeeder::class);
+        $this->call(LimitTypesSeeder::class);
     }
 }

ba255c9dea3b2053dd72382e56136212e8fc9069
antonina <antoninasych@gmail.com>
2019-06-18 11:05:17 +0300

    updates for limits

---
+++ database/migrations/2019_06_18_071121_create_merchant_limits.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMerchantLimits extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('merchant_limits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('merchant_id');
+            $table->foreign('merchant_id')->references('id')->on('merchants');
+            $table->string('amount');
+            $table->enum('limit',['min','max']);
+            $table->string('card_type');
+            $table->integer('limit_types')->unsigned();
+            $table->foreign('limit_types')->references('id')->on('ref_limit_types');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')
+                ->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('merchant_limits');
+    }
+}

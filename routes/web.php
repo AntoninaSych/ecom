7c04cb7daa3602ef4820e8245b53488a545082be
srvadm <A.Drozdenko@concord.ua>
2019-06-20 16:21:44 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,13 @@ Auth::routes();
 
 Route::group(['middleware' => ['log.request']], function () {
 
-
+    Route::get('/', function () {
+        return redirect('/login');
+    });
 
     Route::group(['middleware' => ['auth', 'is.block.user']], function () {
         Route::get('/home', 'HomeController@index')->name('home'); // default page for auth useres
-        Route::get('/', function () {
-            return redirect('/login');
-        });
+
         /**
          * Settings
          */

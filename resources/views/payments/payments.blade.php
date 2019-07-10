f71fb7408a8bf4a962cd161c84cff5cfe2201696
antonina <antoninasych@gmail.com>
2019-07-10 14:39:29 +0300

    try to remove CDN dependencies

--- resources/views/payments/payments.blade.php
+++ resources/views/payments/payments.blade.php
@@ -173,14 +173,13 @@
 @stop
 <script src="{{ asset('/js/libraries/jquery.js') }}"></script>
 <script src="{{ asset('js/libraries/datatables/datatables.min.js') }}"></script>
-{{--<script src="https://code.jquery.com/jquery-3.3.1.js"></script>--}}
-<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
+<script src="{{ asset('js/libraries/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('js/libraries/datatables/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('js/libraries/datatables/buttons.flash.min.js') }}"></script>
+<script src="{{ asset('js/libraries/datatables/jszip.min.js') }}"></script>
+<script src="{{ asset('js/libraries/datatables/pdfmake.min.js') }}"></script>
+<script src="{{ asset('js/libraries/datatables/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('js/libraries/datatables/buttons.print.min.js') }}"></script>
 <script src="{{ asset('js/libraries/datatables/dataTables.bootstrap.min.js') }}"></script>
 
 
--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -4,12 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Backoffice</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
-    <link  href="https://cdn.datatables.net/buttons/1.5.6/css/buttons.dataTables.min.css" rel="stylesheet">
-
-
+     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="stylesheet" href="{{ asset('css/libraries/select2/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/libraries/datatables/buttons.dataTables.min.css') }}">
     <script src="{{ asset('/js/libraries/jquery.js') }}"></script>
     <!-- Bootstrap 3.3.7 -->
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/vendor/bootstrap/dist/css/bootstrap.min.css') }}">
@@ -18,78 +15,46 @@
     <!-- Ionicons -->
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/vendor/Ionicons/css/ionicons.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-
-{{--    @if(config('adminlte.plugins.select2'))--}}
-{{--        <!-- Select2 -->--}}
-{{--        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css">--}}
-{{--    @endif--}}
-
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/AdminLTE.min.css') }}">
 
-
     @if(config('adminlte.plugins.datatables'))
-        <!-- DataTables with bootstrap 3 style -->
-        <link rel="stylesheet" href="//cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.css">
+            <link rel="stylesheet" href="{{ asset('/css/libraries/datatables/dataTables.bootstrap.min.css') }}">
     @endif
 
     @yield('adminlte_css')
-
-
 
     <script src="{{ asset('/js/config.js') }}"></script>
     <script src="{{ asset('/js/libraries/alertify/alertify.min.js') }}"></script>
 
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="{{ asset('/js/libraries/other/html5shiv.min.js')}}"></script>
+    <script src="{{ asset('/js/libraries/other/respond.min.js')}}"></script>
     <![endif]-->
-
-    <!-- Google Font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-</head>
+ </head>
 <body class="hold-transition @yield('body_class')">
 
 @yield('body')
 
-
 <script src="{{ asset('vendor/adminlte/vendor/jquery/dist/jquery.slimscroll.min.js') }}"></script>
 <script src="{{ asset('vendor/adminlte/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-
-
-
-@if(config('adminlte.plugins.select2'))
-    <!-- Select2 -->
-    <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-@endif
-
-@if(config('adminlte.plugins.datatables'))
-    <!-- DataTables with bootstrap 3 renderer -->
-    <script src="//cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.js"></script>
-@endif
 
 @if(config('adminlte.plugins.chartjs'))
     <!-- ChartJS -->
     <script src="{{ asset('vendor/adminlte/vendor/jquery/dist/jquery.min.js') }}"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js"></script>
+    <script src="{{ asset('/js/libraries/chart/Chart.bundle.min.js') }}"></script>
 @endif
 
 @yield('adminlte_js')
 <script src="{{ asset('/js/libraries/jquery-3.3.1.min.js') }}"></script>
-<!-- DatePicker 3.3.7 -->
-
-<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
-<script src="//cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-
+<script src="{{ asset('js/libraries/datatables/datatables.min.js') }}"></script>
+<script src="{{ asset('/js/libraries/moment/moment.js') }}"></script>
+<script src="{{ asset('/js/libraries/datarangepicker/daterangepicker.min.js') }}"></script>
+<link rel="stylesheet" href="{{ asset('/css/libraries/datarangepicker/daterangepicker.css') }}">
 <script src="{{ asset('js/libraries/select2/select2.full.min.js') }}"></script>
-
 <script src="{{ asset('js/libraries/select2/ru.js') }}"></script>
 <!-- DatePicker 3.3.7 -->
 <script src="{{ asset('js/user-list.js') }}"></script>
-
 </body>
 
 </html>
